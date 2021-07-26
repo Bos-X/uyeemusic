24360debd32f84c8efeb63893ff5e7fef2499d6e
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-26 19:48:13 +0700

    setup

--- config.py
+++ config.py
@@ -6,13 +6,19 @@ load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_NAME = getenv("BOT_NAME", "Veez Music Bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
+
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "gcsuppportbots")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "veezasisstant")
+OWNER = getenv("OWNER", "@dlwrml")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
